9c02d6a9c1675674052e5ac9b786b6e7332844ce
Ultra-tech-code <hizick27@gmail.com>
2024-01-18 18:29:52 +0100

    changes

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,9 @@
 // XXX even though ethers is not used in the code below, it's very likely
 // it will be used by any DApp, so we are already including it here
 const { ethers } = require("ethers");
+const { gzip, ungzip } = require("node-gzip");
+var { v4: uuidv4 } = require("uuid");
+var viem = require("viem");
 
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollup_server);
