f73c51c538ff04f9bc5b8054e4c861e67be099a7
Ultra-tech-code <hizick27@gmail.com>
2024-01-18 12:45:18 +0100

    changes made

--- app/src/index.js
+++ app/src/index.js
@@ -4,10 +4,168 @@ const { ethers } = require("ethers");
 
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollup_server);
+const DAPP_ADDRESS_REALY = "0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE";
+let DAPP_ADDRESS = "null";
+
+let profile = {}
+
+  function create_profile(useraddress, name) {
+    if (profile[useraddress] !== undefined) {
+      console.log("profile already exists");
+      return profile[useraddress];
+    }
+
+    profile[useraddress] = {
+      addresss: useraddress,
+      username: name,
+      rank: 0,
+      point: 1000,
+      characters: [],
+    }
+  }
+
+  function get_profile(useraddress) {
+    return profile[useraddress];
+  }
+
 
 async function handle_advance(data) {
   console.log("Received advance request data " + JSON.stringify(data));
+  const payload = data.payload;
+  let JSONpayload = {};
+  try {
+    if (
+      String(data.metadata.msg_sender).toLowerCase() ===
+      DAPP_ADDRESS_REALY.toLowerCase()
+    ) {
+      console.log("setting Dapp address:", payload);
+      DAPP_ADDRESS = payload;
+    }
+
+    const payloadStr = viem.hexToString(payload);
+    JSONpayload = JSON.parse(payloadStr);
+    console.log(`received request ${JSON.stringify(JSONpayload)}`);
+  } catch (e) {
+    console.log("error is:", e);
+    console.log(`Adding notice with binary value "${payload}"`);
+    await fetch(rollup_server + "/report", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ payload: payload }),
+    });
+    return "reject";
+  }
+   
+  let advance_req;
+  try {
+     //{"method":"create_profile","name":"0xblackadam"}
+    if (JSONpayload.method === "create_profile") {
+        console.log("creating profile....");
+        const createdProfile = create_profile(
+          data.metadata.msg_sender,
+          JSONpayload.name
+        );
+        console.log("created profile is:", createdProfile);
+
+        const result = JSON.stringify({ createdProfile: createdProfile });
+        const hexresult = viem.stringToHex(result);
+        console.log("The result is :", hexresult);
+        advance_req = await fetch(rollup_server + "/notice", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ payload: hexresult }),
+        });
+
+        //{"method":"get_profile"}
+    } else if(JSONpayload.method === "get_profile"){
+      console.log("getting  profile....");
+
+    }
+    
+    //{"method":"decompress","id":"000c7899-96bb-498b-8820-691d5e04ba33"}
+    else if (JSONpayload.method === "hash") {
+      console.log("hashing....");
+      const hash = keccak256(toUtf8Bytes(JSONpayload.data));
+      console.log("hash is:", hash);
+
+      advance_req = await fetch(rollup_server + "/notice", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ payload: hash }),
+      });
+
+      //{"method":"mint"}
+    // } else if (JSONpayload.method === "faucet") {
+    //   console.log("sending erc20 tokens.....");
+    //   if (DAPP_ADDRESS === "null") {
+    //     console.log("Dapp address is not set");
+    //     return "reject";
+    //   }
+    //   // console.log("abi is", erc20abi);
+    //   const call = viem.encodeFunctionData({
+    //     abi: erc20abi,
+    //     functionName: "transfer",
+    //     args: [data.metadata.msg_sender, BigInt(JSONpayload.value)],
+    //   });
+    //   let voucher = {
+    //     destination: erc20_contract_address, // dapp Address
+    //     payload: call,
+    //   };
+    //   console.log(voucher);
+    //   advance_req = await fetch(rollup_server + "/voucher", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(voucher),
+    //   });
+    //   console.log("starting a voucher");
+      //{"method":"faucet","value":"150000"}
+    } else {
+      console.log("method undefined");
+      const result = JSON.stringify({
+        error: String("method undefined:" + JSONpayload.method),
+      });
+      const hexresult = viem.stringToHex(result);
+      await fetch(rollup_server + "/report", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+          payload: hexresult,
+        }),
+      });
+    }
+  } catch (e) {
+    console.log("error is:", e);
+    await fetch(rollup_server + "/report", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        payload: viem.stringToHex(JSON.stringify({ error: e })),
+      }),
+    });
+    return "reject";
+  }
+  const json = await advance_req.json();
+  console.log(
+    "Received  status " +
+      advance_req.status +
+      " with body " +
+      JSON.stringify(json)
+  );
   return "accept";
+
 }
 
 async function handle_inspect(data) {
--- app/src/players_profile.js
+++ app/src/players_profile.js
@@ -8,11 +8,12 @@ class Player {
         this.avararURI = avararURI;
         this.characters = [];
         this.id = 0;
-    }
+        this.point = 1000;   
+     }
 
     displayInfo() {
         console.log(`
-            Monika: ${this.name}
+            Monika: ${this.Monika}
             walletAddress: ${this.walletAddress}
             avararURI: ${this.avararURI}
             characters: ${this.characters}
