3584123f712de6ce9972bde37058c32f0149b698
Evans <mr.evans0075@gmail.com>
2024-01-31 14:04:51 +0100

    setting profile page

--- frontend/src/Components/profileForm/Profileform.jsx
+++ frontend/src/Components/profileForm/Profileform.jsx
@@ -4,22 +4,23 @@ import "./profile.css";
 import TeamCard from "../Cards/TeamCard";
 import { Input } from "../../Input";
 
-
 const ContactUsForm = () => {
-  const [dappAddress, setDappAddress] = useState("0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C");
-  const [userName, setUsername] = useState("")
-  const [avatarURI, setAvataruri] = useState("")
-  const [submitClicked, setSubmitClicked] = useState(false); 
+  const [dappAddress, setDappAddress] = useState(
+    "0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C"
+  );
+  const [userName, setUsername] = useState("");
+  const [avatar, setAvatar] = useState(null);
+  const [submitClicked, setSubmitClicked] = useState(false);
 
   const handleSubmit = (e) => {
-    e.preventDefault(); 
-    setSubmitClicked(true); 
+    e.preventDefault();
+    setSubmitClicked(true);
   };
   const functionParamsAsString = JSON.stringify({
     method: "create_profile",
     name: userName,
-    avatarURI: avatarURI,
-  })
+    avatar: avatar,
+  });
 
   return (
     <>
@@ -27,25 +28,55 @@ const ContactUsForm = () => {
         <div className="container flex">
           <div className="content">
             <h6>Create your profile</h6>
-            <p>
-             Are you ready to be a Gamer?
-             Create yoir profile and let's get started 
+            <p className="child-team">
+              Are you ready to be a Gamer? Create your profile and let's get
+              started
             </p>
-            <TeamCard img={"assets/img/team01.png"} name={"KILLER MASTER"} title={"Blockchain Expert"} />
+            <TeamCard
+              img={"assets/img/team01.png"}
+              name={"KILLER MASTER"}
+              title={"Blockchain Expert"}
+              />
+              <p>Game points: {250}</p>
+              <p>No of Charaters: {3}</p>
+              <p>$Nebula balance: {2}</p>
           </div>
 
           <div className="form">
             <form onSubmit={handleSubmit}>
-                <div className="two flex">
-                    <input type="text" placeholder="Enter Your Name*"  id="name"  value={userName}
-                        onChange={(e) => setUsername(e.target.value)}/>
-                    <input type="text" placeholder="Enter Your Avatar Uri*" id="avataruri"   value={avatarURI}
-                        onChange={(e) => setAvataruri(e.target.value)} />
-                </div>
-                <button className="submitButton" type="submit">SUBMIT NOW</button>
+              <div className="two flex">
+                <input
+                  type="text"
+                  placeholder="Enter Your Name*"
+                  id="name"
+                  value={userName}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="file-box">
+                <p>Upload avatar</p>
+                <input
+                  type="file"
+                  alt="profile avatar"
+                  id="avataruri"
+                  accept="image"
+                  // value={avatar}
+                  onChange={(e) => {
+                    const avatar = e.target.files[0];
+                    setAvatar(avatar);
+                  }}
+                />
+              </div>
+              <button className="submitButton" type="submit">
+                SUBMIT NOW
+              </button>
             </form>
             {submitClicked && (
-              <Input dappAddress={dappAddress} input={functionParamsAsString} hexInput={false} />
+              <Input
+                dappAddress={dappAddress}
+                input={functionParamsAsString}
+                hexInput={false}
+              />
             )}
           </div>
         </div>
