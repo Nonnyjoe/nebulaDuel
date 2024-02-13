cfdfa128a8f9bb203669b3e5a4f2144f1f5dd571
Nonnyjoe <idogwuchinonso1@gmail.com>
2024-02-13 21:57:01 +0100

    pre modification

---
+++ app/src/AI_battles.js
@@ -0,0 +1,9 @@
+function select_Characters(players_characters, duel_id) {
+
+}
+
+function select_strategy(players_characters, duel_id) {
+
+}
+
+module.exports = {select_Characters, select_strategy}
