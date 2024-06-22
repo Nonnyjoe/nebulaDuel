83bf7aad8b9b63c0bb3779804299b2267578847a
victor ifeanyi <ifeanyivictor615@gmail.com>
2024-06-22 10:29:03 +0100

    done, adding the background image and character

--- frontend/src/components/arena/GameArena.tsx
+++ frontend/src/components/arena/GameArena.tsx
@@ -1,6 +1,8 @@
 import { ImageWrap } from "../atom/ImageWrap"
 import { Text } from "../atom/Text"
 import { data } from "../profile/PurchaseCharacter"
+import { BattleGround } from "./BattleGround"
+import bg from "/NebulaArena.jpg"
 
 
 const GameArena = () => {
@@ -27,7 +29,12 @@ const GameArena = () => {
             <main className="lg:col-span-3 col-span-2 order-1 lg:order-2 flex flex-col gap-4">
                 <Text as="h3" className="text-4xl font-medium font-belanosima text-center tracking-wider">Arena</Text>
 
-                <div className="w-full h-[450px] bg-myBlack border-4 border-gray-600 battleGround rounded-md"></div>
+                <div className="w-full h-[450px] bg-myBlack border-4 border-gray-600 battleGround rounded-md relative z-0 overflow-hidden">
+                    <ImageWrap image={bg} alt="bg" className="w-full" objectStatus="object-cover" />
+                    <div className="absolute inset-0 flex justify-center items-center z-10">
+                        <BattleGround />
+                    </div>
+                </div>
             </main>
             <aside className="flex order-3 lg:order-3 flex-col gap-4">
                 <Text as="h3" className="md:text-xl text-base mt-6 font-medium font-barlow text-center">Your Opponent</Text>
