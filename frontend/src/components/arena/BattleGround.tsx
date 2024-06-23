048f77e2266bbcee85770d00c7c1bb74ef9721ec
victor ifeanyi <ifeanyivictor615@gmail.com>
2024-06-23 18:21:11 +0100

    added character images

--- frontend/src/components/arena/BattleGround.tsx
+++ frontend/src/components/arena/BattleGround.tsx
@@ -1,27 +1,37 @@
 import { Suspense } from 'react'
 import { Canvas } from "@react-three/fiber";
-// import modelPath from "/models/char1.glb"
-import { useGLTF, } from '@react-three/drei';
+import { useGLTF } from '@react-three/drei';
+import { radToDeg } from 'three/src/math/MathUtils.js';
 
 
-function Model() {
-    const gltf = useGLTF('/models/char1.glb')
-    return <primitive object={gltf.scene} scale={0.22} position={[0.4, -0.6, -0.8]}/>
+
+function Createmodel() {
+    const models = ['/models/char1.glb', '/models/char2.glb', '/models/char3.glb', '/models/char4.glb', '/models/char5.glb'];
+    let element
+    let array1 = []
+    for (let index = 0; index < models.length; index++) {
+        element = models[index];
+        const gltf = useGLTF(element)
+        gltf.scene.lookAt(0,0,0)
+        array1.push(<primitive object={gltf.scene} scale={0.17} rotation={[radToDeg(0), radToDeg(90), radToDeg(0)]} position={[Math.random() * (1.22 - -1.22) + -1.22, Math.random() * (-1.0 - -1.6) + -1.6, -0.8]} />)
+    }
+    array1.forEach(model => {
+        console.log(model.props.position)
+    });
+
+    return (array1)
 }
-
 
 export const BattleGround = () => {
     return (
         <>
-            <Canvas linear flat shadows camera={{ position: [0, 0, 3], fov: 30 }}>
-                {/* <color attach="background" args={["#171720"]} /> */}
+            <Canvas linear flat shadows camera={{ position: [0, 2, 3], fov: 30 }}>
                 <fog attach="fog" args={["#171720", 10, 30]} />
                 <ambientLight intensity={2} />
                 <directionalLight position={[3.3, 1.0, 4.4]} intensity={4} />
                 <Suspense>
-                    <Model />
+                    <Createmodel />
                 </Suspense>
-                {/* <OrbitControls target={[1, 0, 0]} /> */}
             </Canvas>
         </>
     )
