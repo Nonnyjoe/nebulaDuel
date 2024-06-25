92aeb0205feb9c376faf42c015337668f2c405b9
olorunfemibabs <olorunfemisamuel01@gmail.com>
2024-06-25 13:51:05 +0100

    working on join duel page

---
+++ frontend/src/pages/Duels.tsx
@@ -0,0 +1,11 @@
+import JoinDuels from "../components/duels/JoinDuels";
+
+const Duels = () => {
+  return (
+    <main className="w-full flex flex-col">
+      <JoinDuels />
+    </main>
+  );
+};
+
+export default Duels;
