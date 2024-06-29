5662052092d7a04b68aa858b95d6e51eed9734b7
victor ifeanyi <ifeanyivictor615@gmail.com>
2024-06-29 23:03:59 +0100

    resized some models

--- frontend/src/components/arena/BattleGround.tsx
+++ frontend/src/components/arena/BattleGround.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react'
 import { Canvas, useLoader } from "@react-three/fiber";
-import { useGLTF } from '@react-three/drei';
+// import { useGLTF } from '@react-three/drei';
 import { radToDeg } from 'three/src/math/MathUtils.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 
 
 function Createmodel() {
-    const models = ['/models/troll.gltf', '/models/drake.gltf', '/models/techno.gltf', '/models/luna.gltf', '/models/zylar.gltf', '/models/vortex.gltf'];
+    const models = ['/models/troll.gltf', '/models/drake.gltf', '/models/techno.gltf', '/models/luna.gltf', '/models/zylar.gltf', '/models/vortex.gltf', '/models/char1.glb'];
     let element
     let array1 = []
     for (let index = 0; index < models.length; index++) {
@@ -17,7 +17,7 @@ function Createmodel() {
         array1.push(<primitive object={gltf.scene} scale={0.17} rotation={[radToDeg(0), radToDeg(90), radToDeg(0)]} position={[Math.random() * (1.22 - -1.22) + -1.22, Math.random() * (-1.0 - -1.6) + -1.6, -0.8]} />)
     }
     array1.forEach(model => {
-        console.log(model.props)
+        console.log(model)
     });
 
     return (array1)
